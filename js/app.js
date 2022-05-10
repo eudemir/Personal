528c2efc3f185d429520f0d380f74fba7a199141
Egemen Umut Demir <61503328+eudemir@users.noreply.github.com>
2022-05-10 07:24:15 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,25 +1,34 @@
-// NAVIGATION
-
-const navC = document.querySelector('#navC');
-const btn = document.querySelector('.btn');
-const dropDown = document.querySelector('#dropdown');
+// NAV
+const maxW = window.matchMedia("(max-width: 767px)");
+const menuAc = document.querySelector('#nav');
+const btn = document.querySelector('#hamburger');
+const dropDown = document.querySelector('.dropdown');
 const menuLinks = document.querySelector('.bottom');
-
 dropDown.src = 'icons/bars.svg';
-menuLinks.style.display = 'none';
+
+function refresh() {
+    if (maxW.matches) {
+        window.location.reload();
+    }
+}
+
+if (maxW.matches) {
+    menuLinks.style.display = 'none';
+}
 
 btn.addEventListener('click', () => {
     if (menuLinks.style.display == 'none') {
-        navC.style.backgroundColor = 'rgba(26, 26, 26, 0.92)';
-        dropDown.src = 'icons/x-mark.svg';
         menuLinks.style.display = 'flex';
+        menuAc.style.backgroundColor = 'rgba(26, 26, 26, .92';
+        menuAc.style.height = '100vh';
+        dropDown.src = 'icons/x-mark.svg';
     } else {
-        navC.style.backgroundColor = 'transparent';
-        dropDown.src = 'icons/bars.svg';
         menuLinks.style.display = 'none';
+        menuAc.style.height = '15vh';
+        menuAc.style.backgroundColor = 'none';
+        dropDown.src = 'icons/bars.svg';
     }
-});
-
+})
 
 // HEADER
 
@@ -30,18 +39,3 @@ const typeWrite = new Typed('#typeWrite', {
     showCursor: false,
     loop: true
 });
-
-// BACK
-
-VANTA.NET({
-    el: "#backGroundAnimate",
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: false,
-    minHeight: 200.00,
-    minWidth: 200.00,
-    scale: 1.00,
-    scaleMobile: 1.00,
-    backgroundColor: 0x1a1a1a,
-    color: 0xf38320
-})
